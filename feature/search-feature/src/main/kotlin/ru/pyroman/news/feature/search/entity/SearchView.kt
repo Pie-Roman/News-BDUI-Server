759a53ff02936102ad0c589e9d7d6741d40733c7
DoomPI <rslomtev@edu.hse.ru>
2024-05-13 00:01:57 +0300

    добавил custom view поиска

--- feature/search-feature/src/main/kotlin/ru/pyroman/news/feature/search/entity/SearchView.kt
+++ feature/search-feature/src/main/kotlin/ru/pyroman/news/feature/search/entity/SearchView.kt
@@ -1,10 +1,15 @@
 package ru.pyroman.news.feature.search.entity
 
+import divkit.dsl.Custom
 import divkit.dsl.Div
 import divkit.dsl.container
+import divkit.dsl.custom
+import divkit.dsl.customProps
+import divkit.dsl.edgeInsets
 import divkit.dsl.matchParentSize
 import divkit.dsl.scope.DivScope
 import divkit.dsl.vertical
+import divkit.dsl.wrapContentSize
 import ru.pyroman.news.common.view.View
 import ru.pyroman.news.feature.search.SearchConstants.SEARCH_LAYOUT_ID
 
@@ -20,6 +25,26 @@ class SearchView : View() {
             width = matchParentSize(),
             height = matchParentSize(),
             orientation = vertical,
+            paddings = edgeInsets(
+                left = 16,
+                right = 16,
+            ),
+            items = listOf(
+                search() + customProps(
+                    margins = edgeInsets(
+                        top = 30,
+                        bottom = 30,
+                    )
+                ),
+            )
+        )
+    }
+
+    private fun DivScope.search(): Custom {
+        return custom(
+            customType = "search",
+            width = matchParentSize(),
+            height = wrapContentSize(),
         )
     }
 }
